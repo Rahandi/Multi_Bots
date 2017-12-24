469fcbaf5e9b7c1163de0c1727150e6268fae58d
Rahandi <rahandi16@mhs.if.its.ac.id>
2017-12-25 00:25:59 +0700

    coba postback

--- app.py
+++ app.py
@@ -515,8 +515,8 @@ def handle_join(event):
         data['alt'] = 'Multi_Bots Joined'
         data['tumbnail'] = None
         data['title'] = None
-        data['text'] = 'Terimakasih telah mengundang !!!'
-        data['action'] = [actionBuilder(1, ['postback'], ['tap untuk bantuan penggunaan'], ['help'])]
+        data['text'] = 'Terimakasih telah mengundang !!!\ntap untuk bantuan penggunaan'
+        data['action'] = [actionBuilder(1, ['postback'], ['tap !!!'], ['help'])]
         replyTemplateMessage(reply_token, data)
     except LineBotApiError as e:
         print(e.status_code)
