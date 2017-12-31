95e4e53b93da55923ffe411ca9eced382316c252
Rahandi <rahandi16@mhs.if.its.ac.id>
2017-12-31 13:27:22 +0700

    fix memegen

--- app.py
+++ app.py
@@ -791,7 +791,7 @@ def tebakgambar(token, msgid):
             persenan = float(data[a]['value']) * 100
             persenan = format(persenan, '.2f')
             persenan = persenan + '%'
-            kata += '\n%s     %s' % (data[a]['name'], persenan)
+            kata += '\n%s\t%s' % (data[a]['name'], persenan)
         replyTextMessage(token, str(kata))
     except Exception as e:
         raise e
