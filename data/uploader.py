b477f6a393eb9ff1552d4913dfab57ccae1b5359
Rahandi <rahandi16@mhs.if.its.ac.id>
2018-01-14 02:44:39 +0700

    some

--- data/uploader.py
+++ data/uploader.py
@@ -1,15 +1,15 @@
 import requests
 
 class Uploader():
-    def upload(path):
+    def upload(self, path):
         files = {'file':open(path, 'rb')}
         return requests.post('https://d1.dropfile.to/upload', files=files).json()
 
-    def status(url):
+    def status(self, url):
         url = url.replace('https://dropfile.to/', 'https://dropfile.to/api/')
         return requests.get(url).json()
 
-    def delete(url, key):
+    def delete(self, url, key):
         url = url.replace('https://dropfile.to/', 'https://dropfile.to/api/')
         url = url + '?delete=' + key
         return requests.get(url).json()
