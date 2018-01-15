637fbdda0749d9261379dc388a19bc4a939e4f1b
Rahandi <rahandi16@mhs.if.its.ac.id>
2018-01-15 23:57:48 +0700

    'log'

--- app.py
+++ app.py
@@ -554,8 +554,8 @@ def devian(token, mode, berapa, query=None):
             tipe = 'img'
             for a in range(len(listpict)):
                 isi_TB = {}
-                isi_TB['tumbnail'] = listpict[a]
-                isi_TB['action'] = actionBuilder(1, ['uri'], ['direct link'], [listpict[a]])
+                isi_TB['tumbnail'] = listpict[a].replace('http://', 'https://')
+                isi_TB['action'] = actionBuilder(1, ['uri'], ['direct link'], [listpict[a].replace('http://', 'https://')])
                 TB.append(isi_TB)
             dat = {}
             dat['alt'] = 'Multi_Bots Deviantart Search'
@@ -579,8 +579,8 @@ def devian(token, mode, berapa, query=None):
             tipe = 'img'
             for a in range(len(listpict)):
                 isi_TB = {}
-                isi_TB['tumbnail'] = listpict[a]
-                isi_TB['action'] = actionBuilder(1, ['uri'], ['direct link'], [listpict[a]])
+                isi_TB['tumbnail'] = listpict[a].replace('http://', 'https://')
+                isi_TB['action'] = actionBuilder(1, ['uri'], ['direct link'], [listpict[a].replace('http://', 'https://')])
                 TB.append(isi_TB)
             dat = {}
             dat['alt'] = 'Multi_Bots Deviantart Hot'
